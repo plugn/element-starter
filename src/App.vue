8c84b962ff967cdfd848bd7529cd84f6c54dcfb0
Maxim Dolgov <bananafishbone@gmail.com>
2017-09-20 18:00:02 +0300

    theme customizing

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <el-button @click.native="startHacking">Yes!</el-button>
-  </div>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<h1>{{ msg }}</h1>
+
+		<el-button @click.native="startHacking">Yes!</el-button>
+
+		<Tabs></Tabs>
+	</div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Want something new?'
-    }
-  },
+	import Tabs from './Tabs.vue'
 
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'Shhh',
-        message: 'Just be patient...',
-        duration: 6000
-      })
-    }
-  }
-}
+	export default {
+		components: {
+			Tabs
+		},
+		data() {
+			return {
+				msg: 'Want something new?'
+			}
+		},
+
+		methods: {
+			startHacking() {
+				this.$notify({
+					title: 'Shhh',
+					message: 'Just be patient...',
+					duration: 6000
+				})
+			}
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
+	#app {
+		font-family: Helvetica, sans-serif;
+		text-align: center;
+	}
 </style>
